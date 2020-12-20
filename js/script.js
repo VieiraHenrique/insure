d74dd6457972e221d8b024202e250bb8d2c3195c
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2020-12-20 22:05:48 +0100

    add hover to social icons and prevent scroll from menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const toggle = document.querySelector('.toggle');
 const menu = document.getElementById('menu');
+const body = document.querySelector('body');
+let state = 0;
 
 toggle.addEventListener('click', () => {
 	menu.classList.toggle('show');
 	toggle.classList.toggle('close');
+	body.classList.toggle('noscroll');
 });
